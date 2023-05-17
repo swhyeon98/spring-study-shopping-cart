ba3e51c4ec164920c9168fb24402c84afae676eb
swhyeon98 <smreo75@gmail.com>
2023-05-17 19:30:46 +0900

    refactor: ImageUrl 상수를 하나로 통합

--- src/main/java/cart/domain/ImageUrl.java
+++ src/main/java/cart/domain/ImageUrl.java
@@ -4,9 +4,7 @@ public class ImageUrl {
     private static final String ERROR_MESSAGE = "[ERROR] ";
     private static final String URL_EMPTY_ERROR = "이미지 URL이 비어있습니다. 이미지 URL을 입력해주세요.";
     private static final String URL_FORMAT_ERROR = "유효하지 않은 이미지 형식입니다.";
-    private static final String VALID_URL_JPG_FORMAT = ".jpg";
-    private static final String VALID_URL_JPEG_FORMAT = ".jpeg";
-    private static final String VALID_URL_PNG_FORMAT = ".png";
+    private static final String VALID_URL_REGEX = ".*\\.(jpg|jpeg|png)$";
 
     private final String value;
 
@@ -27,13 +25,7 @@ public class ImageUrl {
     }
 
     private void validateUrlFormat(String value) {
-        if (!value.matches(VALID_URL_JPG_FORMAT)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + URL_FORMAT_ERROR);
-        }
-        if (!value.matches(VALID_URL_JPEG_FORMAT)) {
-            throw new IllegalArgumentException(ERROR_MESSAGE + URL_FORMAT_ERROR);
-        }
-        if (!value.matches(VALID_URL_PNG_FORMAT)) {
+        if (!value.matches(VALID_URL_REGEX)) {
             throw new IllegalArgumentException(ERROR_MESSAGE + URL_FORMAT_ERROR);
         }
     }
